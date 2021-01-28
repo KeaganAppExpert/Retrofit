6fdca43be6d2ea3bd0ccbd60c0b89dff1a04aca1
keagan.gonsalves@appexperts.me.uk <Hatfield2021!>
2021-01-28 16:37:24 +0000

    First commit

---
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.retrofit.data.remote.SOService;
+import com.example.retrofit.model.Item;
+import com.example.retrofit.model.SOAnswersResponse;
+import com.example.retrofit.utils.ApiUtils;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private AnswersAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+    private SOService mService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mService = ApiUtils.getSOService();
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_answers);
+        mAdapter = new AnswersAdapter(this, new ArrayList<Item>(0), new AnswersAdapter.PostItemListener() {
+
+            @Override
+            public void onPostClick(long id) {
+                Toast.makeText(MainActivity.this, "Post id is" + id, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(itemDecoration);
+
+        loadAnswers();
+    }
+
+    private void loadAnswers() {
+        mService.getAnswers().enqueue(new Callback<SOAnswersResponse>() {
+            @Override
+            public void onResponse(Call<SOAnswersResponse> call, Response<SOAnswersResponse> response) {
+
+                if(response.isSuccessful()) {
+                    mAdapter.updateAnswers(response.body().getItems());
+                    Log.d("MainActivity", "posts loaded from API");
+                }else {
+                    int statusCode  = response.code();
+                    // handle request errors depending on status code
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SOAnswersResponse> call, Throwable t) {
+                //showErrorMessage();
+                Log.d("MainActivity", "error loading from API");
+
+            }
+        });
+    }
+}
